9b26a641c9831dd96b6834fd67c0ca384f25aae6
Paolice <paolice@oli-core.com>
2026-01-31 09:05:48 +0100

    Implémentation Système Identité et Abonnement

--- src/routes/subscription.routes.js
+++ src/routes/subscription.routes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const subscriptionController = require('../controllers/subscription.controller');
-const authMiddleware = require('../middlewares/auth.middleware');
+const { requireAuth } = require('../middlewares/auth.middleware');
 
 // Routes protégées par Auth
-router.use(authMiddleware);
+router.use(requireAuth);
 
 router.post('/upgrade', subscriptionController.upgradeAccount);
 router.get('/status', subscriptionController.getStatus);
